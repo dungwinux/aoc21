9a947f63f86ac3faed4fad9d6eb44213e6dbb6af
Dung Tuan Nguyen <ntddebugger@gmail.com>
2021-12-02 21:04:29 -0500

    add: modulize running code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use std::fs;
 mod day02;
-use day02::{p1::solve, p2::solve as solve2};
+use day02::{p1, p2};
+
+fn run(contents: Vec<&str>, solver: &dyn Fn(Vec<&str>) -> Result<i32, Box<dyn std::error::Error>>) {
+    match solver(contents) {
+        Ok(answer) => println!("Answer: {}", answer),
+        Err(problem) => println!("We got a problem: {}", problem),
+    }
+}
 
 fn main() {
     let base = String::from("src/day02/");
@@ -8,9 +15,6 @@ fn main() {
         fs::read_to_string(base + "input.txt").expect("Something went wrong reading the file");
 
     let contents: Vec<&str> = data.split_terminator("\n").collect();
-
-    match solve2(contents) {
-        Ok(answer) => println!("Answer: {}", answer),
-        Err(problem) => println!("We got a problem: {}", problem),
-    }
+    run(contents.clone(), &p1::solve);
+    run(contents, &p2::solve);
 }
