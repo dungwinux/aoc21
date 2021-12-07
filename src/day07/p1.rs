4413bdd67425ea07607220d3a2198cbacfd562cf
Dung Tuan Nguyen <ntddebugger@gmail.com>
2021-12-07 00:38:30 -0500

    fix: remove unnecessary index field

--- src/day07/p1.rs
+++ src/day07/p1.rs
@@ -12,7 +12,7 @@ pub fn solve(inp: Vec<&str>) -> Result<i64, Box<dyn std::error::Error>> {
         .enumerate()
         .filter(|x| x.1 != 0)
         .collect();
-    println!("{:?}", position);
+    // println!("{:?}", position);
     let mut collector = vec![0; 2000];
     for i in 0..collector.len() {
         for item in &position {
@@ -20,11 +20,6 @@ pub fn solve(inp: Vec<&str>) -> Result<i64, Box<dyn std::error::Error>> {
         }
     }
     println!("{:?}", collector);
-    let global_min = collector
-        .into_iter()
-        .enumerate()
-        .min_by(|x, y| x.1.cmp(&y.1))
-        .unwrap();
-    println!("{:?}", global_min.1);
-    Ok(global_min.1 as i64)
+    let global_min = collector.into_iter().min().unwrap();
+    Ok(global_min)
 }
